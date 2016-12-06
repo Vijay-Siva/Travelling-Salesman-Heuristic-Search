73630b92a587ba0f01ac281a25d00e2c6f8f5fc4
Vijay Siva <vijay_732@hotmail.com>
2016-12-06 00:46:58 -0500

    refactored tsp.py to be subclass StateSpace

--- tsp.py
+++ tsp.py
@@ -1,12 +1,36 @@
-python.linting.enabled = False
+#python.linting.enabled = False
 
 from search import * 
-class TSP():
-   def __init__(self, vertices, edges, path=[]):
-       self.order = len(vertices) 
-       self.vertices = vertices 
-       self.edges = edges
-       self.path = path
+
+class TSP(StateSpace):
+    def __init__(self, action, gval, parent, vertices, edges, path):
+        StateSpace.__init__(self, action, gval, parent)
+        self.order = len(vertices) 
+        self.vertices = vertices 
+        self.edges = edges
+        self.path = path
+        return
+    
+    def successors(self):
+        successors = [] 
+        visited = Set()
+        for edges in self.path:
+            visited.add(edge[0])
+        
+        visited.add(self.path[-1][1])
+
+        unvisited = [x for x in self.vertices and x not in visited]
+
+        for vertex in visited:
+            for edge in self.get_edges(vertex):
+                if (edge[1] in unvisited):
+                    new_state = TSP(str(edge), self.gval + edge[2], self, self.vertices, self.edges, path + [edge])
+                    successors.append(new_state)
+
+        return successors 
+
+    def hashable_state(self):
+        return hash((self.vertices, self.edges, self.path))
     
     '''Return all the outgoing edges for the given vertex in self.'''
     def get_edges(self, vertex):
