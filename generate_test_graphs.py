1a66a5d26f96d5ef1194f4235a3f2cdf05f8c789
Theo Poenaru <theo@Theos-MacBook-Air.local>
2016-12-07 15:07:02 -0500

    randomized graph (thru vertex order in gen)

--- generate_test_graphs.py
+++ generate_test_graphs.py
@@ -1,33 +1,26 @@
-from random import random
+from random import random, shuffle
 from math import ceil
+
+MAX_WEIGHT = 5
 
 E = []
 V = []
 
+# generate a list of verices in random order
+for i in range(49):
+    V.append(i+1)
+V.append(i+2)
+shuffle(V)
+
+# create trivial Hamiltonian cycle (connect all vertices
+# with edges of weight 1)
+for i in range(49):
+    E.append((V[i], V[i+1], 1))
+E.append((V[i+1], V[0], 1))
 
 for i in range(49):
-    E.append(i+1)
-E.append(i+2)
-
-# add Hamiltonian cycle
-for i in range(49):
-    E.append(i+1)
-    V.append([i+1, i+2, ceil(5*random())])
-
-# add Hamiltonian cycle
-for i in range(49):
-    E.append(i+1)
-    V.append([i+1, i+2, ceil(5*random())])
-
-# add Hamiltonian cycle
-for i in range(49):
-    E.append(i+1)
-    V.append([i+1, i+2, ceil(5*random())])
-
-
-# add noise
-for i in range(50):
-    V.append([ceil(50*random()), ceil(50*random()), ceil(5*random())])
+    E.append((V[i], V[i+1], ceil(MAX_WEIGHT*random())))
+E.append((V[i+1], V[0], ceil(MAX_WEIGHT*random())))
 
 print(E)
 print(V)
